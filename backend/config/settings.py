374c738c54677839a6cb6df20806909953a957f5
Anton Grabovsky <agrabovsky2020@gmail.com>
2021-02-09 10:15:34 +0300

    Backend: add app

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'media_server.apps.MediaServerConfig'
 ]
 
 MIDDLEWARE = [
