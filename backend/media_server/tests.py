8422ee434ed1fd384dc7b73940610351ed94763a
Anton Grabovsky <agrabovsky2020@gmail.com>
2021-02-09 12:42:43 +0300

    Backend: add testing of the slug creation

--- backend/media_server/tests.py
+++ backend/media_server/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+from media_server.models import Genre
+
+
+class GenreModelTests(TestCase):
+    def test_create_unique_slug(self):
+        genre1 = Genre.objects.create(name='genre 1')
+        self.assertEqual(genre1.slug, 'genre-1')
+
+        genre2 = Genre.objects.create(name='genre 1')
+        self.assertNotEqual(genre2.slug, 'genre-1')
