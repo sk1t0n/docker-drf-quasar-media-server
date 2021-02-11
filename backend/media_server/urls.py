20d499515e956dc8db9eaa81527abfd93b2e9dd7
Anton Grabovsky <agrabovsky2020@gmail.com>
2021-02-11 15:07:21 +0300

    Backend: add routes for videos and genres

---
+++ backend/media_server/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from media_server import views
+
+urlpatterns = [
+    path('videos/', views.VideoListCreateView.as_view(), name='video_list_create'),
+    path(
+        'videos/<slug:slug>/',
+        views.VideoRetrieveUpdateDestroyView.as_view(),
+        name='video_read_update_delete'
+    ),
+    path('genres/', views.GenreListCreateView.as_view(), name='genre_list_create'),
+    path(
+        'genres/<slug:slug>/',
+        views.GenreRetrieveUpdateDestroyView.as_view(),
+        name='genre_read_update_delete'
+    )
+]
