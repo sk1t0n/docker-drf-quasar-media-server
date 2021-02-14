89538165a1ba33c4e463b50db50befd74a7669e9
Anton Grabovsky <agrabovsky2020@gmail.com>
2021-02-14 10:42:13 +0300

    Frontend: add operations on video to Vuex Store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-// import example from './module-example'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
@@ -16,13 +16,73 @@ Vue.use(Vuex)
 
 export default function (/* { ssrContext } */) {
   const Store = new Vuex.Store({
-    modules: {
-      // example
+    state: {
+      countVideos: 0,
+      urlNextVideos: 'http://127.0.0.1:8080/api/videos/?page=1',
+      urlPrevVideos: null,
+      videos: [],
+      video: {}
+    },
+
+    getters: {
+      getCountVideos: state => state.countVideos,
+      getUrlNextVideos: state => state.urlNextVideos,
+      getUrlPrevVideos: state => state.urlPrevVideos,
+      getVideos: state => state.videos,
+      getVideo: state => state.video
+    },
+
+    mutations: {
+      updateCountVideos (state, countVideos) {
+        state.countVideos = countVideos
+      },
+      updateUrlNextVideos (state, url) {
+        state.urlNextVideos = url
+      },
+      updateUrlPrevVideos (state, url) {
+        state.urlPrevVideos = url
+      },
+      updateVideos (state, videos) {
+        state.videos = videos
+      },
+      updateVideo (state, video) {
+        state.video = video
+      }
+    },
+
+    actions: {
+      loadVideos (context, url) {
+        if (!url) return
+        const arr = url.split('?')
+        url = arr[0]
+        let page
+        if (arr.length === 2 && arr[1].startsWith('page')) {
+          page = arr[1].split('=')[1]
+        } else {
+          page = 1
+        }
+        axios.get(url, { params: { page: page } })
+          .then(response => {
+            if (response.status === 200) {
+              context.commit('updateCountVideos', response.data.count)
+              context.commit('updateUrlNextVideos', response.data.next)
+              if (!response.data.previous || response.data.previous.includes('?page=')) {
+                context.commit('updateUrlPrevVideos', response.data.previous)
+              } else {
+                context.commit('updateUrlPrevVideos', `${response.data.previous}?page=${page - 1}`)
+              }
+              context.commit('updateVideos', response.data.results)
+            }
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      }
     },
 
     // enable strict mode (adds overhead!)
     // for dev mode only
-    strict: process.env.DEBUGGING
+    strict: process.env.DEV
   })
 
   return Store
